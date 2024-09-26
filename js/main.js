99c1173bf5f58389d975ce1b46457efc457f6f9e
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-09-26 16:21:09 +0300

    Add navbar and smooth scrolling functionality with styling
    - Implement CSS for smooth scrolling and navigation hover effects.
    - Add responsive navbar with Bootstrap and FontAwesome icons.
    - Adjust body height and layout to accommodate new elements.
    - Integrate JavaScript for future scroll behavior handling in the navbar.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("hi")
+/**
+ * navbar scroll
+ */
+// const navbar = document.getElementById("main-navbar")
+// window.addEventListener('scroll', function () {
+//     if (window.pageYOffset > 0) {
+//         navbar.classList.add("navbar-after-scroll")
+//     } else {
+//         navbar.classList.remove("navbar-after-scroll")
+//     }
+// })
