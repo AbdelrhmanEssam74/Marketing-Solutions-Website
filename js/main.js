2a7189d93db61a84e71ce30e4586464e9247a619
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-09-27 18:29:02 +0300

    Add responsive styles, navigation bar, and back-to-top button
    - Implement CSS styles for smooth scrolling, responsive design, and layout enhancements.
    - Create a responsive navigation bar with an offcanvas sidebar.
    - Add a back-to-top button for improved user navigation.
    - Include JavaScript functionality for displaying the back-to-top button based on scroll position.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-/**
- * navbar scroll
- */
-// const navbar = document.getElementById("main-navbar")
-// window.addEventListener('scroll', function () {
-//     if (window.pageYOffset > 0) {
-//         navbar.classList.add("navbar-after-scroll")
-//     } else {
-//         navbar.classList.remove("navbar-after-scroll")
-//     }
-// })
+$(document).ready(function() {
+    let $button = $("#btn-back-to-top");
+
+    $(window).on("scroll", function() {
+        scrollFunction();
+    });
+
+    function scrollFunction() {
+        if ($(document).scrollTop() > 20 || $(document).scrollTop() > 20) {
+            $button.css("display", "block");
+        } else {
+            $button.css("display", "none");
+        }
+    }
+
+    $button.on("click", function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+});
